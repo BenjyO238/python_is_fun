d8bbef2ae825cc0a98826fa20bc25668d85f07d7
Benjy Orlowitz <benjyorlowitz@Benjys-MacBook-Pro.local>
2024-07-11 18:05:46 -0400

    Work on student workspace.

--- asm_python_course/student_workspace/no_main_method.py
+++ asm_python_course/student_workspace/no_main_method.py
@@ -1,8 +1,9 @@
 import random
-import yaml
 
 
 
 
 sample_text = "The quick brown fox jumps over the lazy dog."
 sample_list = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "grape"]
+
+print("Hello, World!")

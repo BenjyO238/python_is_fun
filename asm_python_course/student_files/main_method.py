94aed793e70c18b0d99f3e7b4300558ff93136ca
Benjy Orlowitz <benjyorlowitz@Benjys-MacBook-Pro.local>
2024-07-11 17:45:26 -0400

    Adding student workspace.

---
+++ asm_python_course/student_files/main_method.py
@@ -0,0 +1,14 @@
+import random
+
+sample_text = "The quick brown fox jumps over the lazy dog."
+sample_list = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "grape"]
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print("Hello, World!")
---
+++ asm_python_course/student_files/no_main_method.py
@@ -0,0 +1,8 @@
+import random
+import yaml
+
+
+
+
+sample_text = "The quick brown fox jumps over the lazy dog."
+sample_list = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "grape"]
